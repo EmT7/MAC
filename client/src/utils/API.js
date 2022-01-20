e4b650ffdb698b3e02574e1ece4d04e056a90e4c
Miranda <88161424+MirandaT77@users.noreply.github.com>
2022-01-19 19:31:55 -0800

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,3 @@
-
 export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  return fetch(`https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&orderBy=title&apikey=bc50bff54553e9f96a24b7f680076b5c6c040488`);
 };
