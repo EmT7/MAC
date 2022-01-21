f7eaf459df99c7e47b9693a8f75853aaa6f1644d
Cienna97 <cmortenson97@gmail.com>
2022-01-21 09:00:08 -0700

    push

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,18 @@
 //import React, { Component } from 'react';
 //import md5 from 'js-md5';
-
+//REACT_APP_MARVEL_PUBLIC_API_KEY
+//let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
 //document.querySelector(#app).innerHTML = startOfDay(new Date)
 export const  searchMarvelComics = (query) => {
 //let ts = "1234"
-let publicKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
-let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
 
 
-return fetch( `http://gateway.marvel.com:443/v1/public/comics?ts=1234&apikey=publicKey&hash=md5(3623f846ef760096e1a1cd400518cbba)${query}`);
+
+return fetch( `http://gateway.marvel.com:443/v1/public/comics?ts=1234&apikey={REACT_APP_MARVEL_PUBLIC_API_KEY}&hash=md5(3623f846ef760096e1a1cd400518cbba)${query}`);
 };
 //let hash = md5(ts + privateKey + publicKey)
+
+
 
 
 //console.log(hash) 
@@ -26,16 +28,7 @@ return fetch( `http://gateway.marvel.com:443/v1/public/comics?ts=1234&apikey=pub
 
   //return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 
- // async function loadHeroesCharacters() {
-    //const ts = Number(new Date());
-    //const hash = md5.create();
-    //hash.update(ts + PRIVATE_KEY + PUBLIC_KEY);
-   // try {
-      //const response = await fetch(
-        //`https://gateway.marvel.com/v1/public/characters?ts=${ts}&orderBy=name&limit=10&apikey=97a0ce4758cb2a37f84f38a6943e1bf5&hash=${hash.h ex()}
-  
-  //return fetch ("http://gateway.marvel.com/v1/public/comics?");
-//};
+
 
 
 
