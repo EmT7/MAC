5c2cef91b95a476dbe3df2e6d606dc91574a356e
Cienna97 <cmortenson97@gmail.com>
2022-01-21 23:21:00 -0800

    api cleaning up to use marvel api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,8 +4,12 @@ export const  searchMarvelComics = (query) => {
 
 
     let ts = "1234"
-    let publicKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
-    let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
+    //let publicKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
+    let publicKey = "98857d651c6ba3b8d6f8b8a58cbc5ab0"
+    //console.log(`publicKey: ${publicKey}`)
+    //let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
+    let privateKey = "f26733a20862b3ea118ed7a95e1db43e08601384"
+    //console.log(`privateKey: ${privateKey}`)
     let hash = md5(ts + privateKey + publicKey)
 
     console.log(hash)
